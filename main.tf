3624388fb786c45fb4876dd1b44f9ff8e7c29aea
sylvan <sylvanchouhan@gmail.com>
2025-09-09 15:47:53 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "vpn" {
+  name      = var.droplet_name
+  region    = var.region
+  size      = "s-1vcpu-1gb"
+  image     = "199332865"
+  ssh_keys  = [var.ssh_fingerprint]
+  tags      = ["wireguard"]
+}
+
+output "vpn_ip" {
+  value = digitalocean_droplet.vpn.ipv4_address
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "do_token" {}
+variable "ssh_fingerprint" {}
+variable "region" {
+  default = "nyc3"
+}
+variable "droplet_name" {
+  default = "wireguard-vpn"
+}
